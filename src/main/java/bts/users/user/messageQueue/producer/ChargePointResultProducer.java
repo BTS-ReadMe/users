1696363ea591dcb3dff2b3fbe2aadb96d6351414
logwns <logwns@gmail.com>
2023-06-05 12:14:21 +0900

    [UPDATE] #BTS-94 : refactor - 디렉토리 구조, 불필요한 서비스 제거 등 리팩토링했습니다 - 중간에 커밋을 조금씩 했어야됐는데 죄송합니다

--- src/main/java/bts/users/user/messageQueue/producer/ChargePointResultProducer.java
+++ src/main/java/bts/users/user/messageQueue/producer/ChargePointResultProducer.java
@@ -1,29 +1,30 @@
-package bts.users.user.service.producer;
+package bts.users.user.messageQueue.producer;
 
 import bts.users.dto.ChargePointResultDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class SendChargePointResult {
+public class ChargePointResultProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void sendResult(ChargePointResultDto chargePointResultDto) {
+    public void sendChargePointResult(ChargePointResultDto chargePointResultDto) {
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
-        String data = "";
 
         try {
-            data = mapper.writeValueAsString(chargePointResultDto);
+            String data = mapper.writeValueAsString(chargePointResultDto);
+            kafkaTemplate.send("chargePointResult", data);
         } catch (Exception e) {
-            return;
+            log.info(e.toString());
         }
-
-        kafkaTemplate.send("chargePointResult", data);
     }
 }
