e0324fedd64c8f7b7eb3fb42483cad39fa821861
logwns <logwns@gmail.com>
2023-06-09 15:28:42 +0900

    [CREATE] #BTS-175 : get user point - 유저의 포인트가 얼마나 있는지 조회하기를 했습니다

--- src/main/java/bts/users/user/service/UserService.java
+++ src/main/java/bts/users/user/service/UserService.java
@@ -3,6 +3,7 @@ package bts.users.user.service;
 import bts.users.user.requestObject.RequestAdminLogin;
 import bts.users.user.responseObject.Message;
 import bts.users.user.responseObject.ResponseAdminLogin;
+import bts.users.user.responseObject.ResponseGetPoint;
 import bts.users.user.responseObject.ResponseLogin;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
@@ -12,4 +13,6 @@ public interface UserService {
     public ResponseEntity<Message<ResponseLogin>> login(String code);
 
     public ResponseEntity<Message<ResponseAdminLogin>> adminLogin(RequestAdminLogin requestAdminLogin);
+
+    public ResponseEntity<Message<ResponseGetPoint>> getPoint(String uuid);
 }
